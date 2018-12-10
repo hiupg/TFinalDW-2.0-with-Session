d362f3d44cfbd8acd2212f4e6a8c33cd850710e7
teste <teste@teste.com>
2018-12-10 16:12:27 -0200

    att03

--- TelaCadastroFuncionario/cadastroFuncionario.php
+++ TelaCadastroFuncionario/cadastroFuncionario.php
@@ -1,14 +1,3 @@
-<?php
-session_start();
-
-if (isset($_SESSION["login"])){
-    if ($_SESSION["login"]==false){
-        header("location: login.php");
-    }
-}else{
-    header("location: login.php");
-}
-?>
 <!DOCTYPE html>
 <html>
     <head>
